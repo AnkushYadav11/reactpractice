abe807fee77d1471946f5fe5dbb7a6166fbd8bca
Ankush Yadav <yadavankush1811@gmail.com>
2025-09-19 11:59:54 +0530

    Dark Mode function created.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Textutils from './components/Textutils';
+import React from 'react';
 
 function App() {
+  const [mode, setMode] = useState('white');
+  const [btnText, setBtnText] = useState('Enable Dark Mode');
+
+  const myToggal = () =>{
+    if(mode === 'white'){
+      setMode('dark')
+      setBtnText('Enabel Light Mode')
+      document.body.style.backgroundColor = 'gray'
+    }
+    else{
+      setMode('white')
+      setBtnText('Enabel Dark Mode')
+      document.body.style.backgroundColor = 'white'
+    }
+  }
+
   return (
     <>
-    <Navbar title="Welcome"/>
-    <Textutils/>
+    <Navbar title="Welcome" mode={mode} myStyle={myToggal} btnText={btnText}/>
+    <Textutils mode={mode}/>
     </>
   );
 }
--- src/components/Textutils.js
+++ src/components/Textutils.js
@@ -1,41 +1,24 @@
 import React, { useState } from 'react';
 
-export default function Textutils() {
+export default function Textutils(props) {
     const [text, setText] = useState('');
-    const [btntext,setbtnText] = useState("Enable Dark Mode.")
-    const [color, setColor] = useState({
-        color: "black",
-        background: "white"
-    });
 
-    const toggal = () => {
-        if (color.color === "black") {
-            setColor({
-                color: "white",
-                background: "black"
-            })
-            setbtnText("Enable Light Mode");
-        }
-        else {
-            setColor({
-                color: "black",
-                background: "white"
-            })
-            setbtnText("Enable Dark Mode");
-        }
-    }
     return (
         <>
-            <div className='d-flex flex-column justify-content-center' style={color}>
-                <div className=''>
+            <div className='d-flex flex-column justify-content-center' style={{
+                backgroundColor: props.mode === 'white' ? 'white' : 'gray', color: props.mode === 'white' ? 'black' : 'white'
+            }}>
+                <div>
                     <h1>Enter Your Text hear</h1>
-                    <textarea style={color} cols="100" rows="10" placeholder='Enter Your Text Here.' value={text} onChange={(e) => { setText(e.target.value) }}></textarea>
+                    <textarea cols="100" rows="10" style={{
+                        backgroundColor: props.mode === 'white' ? 'white' : 'gray',
+                        color: props.mode === 'white' ? 'balck' : 'white', borderColor: props.mode === 'white' ? 'black' : 'white'
+                    }} placeholder='Enter Your Text Here.' value={text} onChange={(e) => { setText(e.target.value) }}></textarea>
                 </div>
                 <div>
                     <button className='btn btn-primary m-2' onClick={() => { let a = text.toUpperCase(); setText(a) }}>Upper</button>
                     <button className='btn btn-secondary m-2' onClick={() => { let a = text.toLowerCase(); setText(a) }}>Lower</button>
                     <button className='btn btn-danger m-2' onClick={() => { let text = ''; setText(text) }}>Clear</button>
-                    <button className='btn btn-outline-primary m-2' onClick={toggal}>{btntext}</button>
                     <h5>Number of chars {text.length}</h5>
                     <h5>Time to read {0.008 * text.split(" ").length} minutes</h5>
                     <h5>Number of word {text.split(' ').length - 1}</h5>
